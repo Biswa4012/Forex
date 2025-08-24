46f767d471f6ed3a8873cc57243d3f2d9fafbd5e
Biswa4012 <112005491+Biswa4012@users.noreply.github.com>
2025-08-24 15:47:07 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // This imports the code from the Canvas
+import App from './App.jsx';
 
 const rootElement = document.getElementById('root');
 
