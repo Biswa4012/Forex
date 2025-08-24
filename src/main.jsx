dc16a3709f6a644ce3d78775d7245a83011083ac
Biswa4012 <112005491+Biswa4012@users.noreply.github.com>
2025-08-24 15:40:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
-npm install react react-dom
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'; // This imports the code from the Canvas
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
